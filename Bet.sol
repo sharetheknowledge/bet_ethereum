a1e036ca4f5d1381005e3d828b163fb7c3191669
Daniel Weigel <daniel.wgel@gmail.com>
2021-09-19 08:12:43 +1200

    First Bet.sol template ready

--- Bet.sol
+++ Bet.sol
@@ -5,26 +5,56 @@ contract Bet{
     string public description;
     uint public contribution;
     address public better;
-    bool public managerIsCorrect;
+    uint public dummyBalance;
+
 
 
 
     function Bet(string desc,uint valueStaked) public payable{
-        msg.value==valueStaked;
+        require(msg.value==valueStaked);
+        // msg.value==valueStaked;
         manager=msg.sender;
         description=desc;
         contribution=valueStaked;
 
+
+
     }
+
+    modifier restricted(){
+        require(msg.sender==manager);
+        _;
+    }
+
+
+
 
     function contribute() public payable{
         require(msg.value==contribution);
+        require(better==address(0));
         better=msg.sender;
 
     }
 
-    // function isManagerright(bool managerCorrect) public {
+    function isManageRight(bool managerCorrect) public payable restricted {
 
-    // }
+        if(manager==better || better==address(0)){
+            manager.transfer(this.balance);
+        }
+
+        if (managerCorrect == true) {
+            manager.transfer(this.balance);
+
+        } else if (managerCorrect==false) {
+            better.transfer(this.balance);
+        }
+
+    }
+
+    function viewBalance() public view returns(uint){
+    return this.balance;
+  }
+
+
 
 }
